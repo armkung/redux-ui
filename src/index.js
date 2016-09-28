8d5894d8ca41eca6bb62a0da00280fa221069a12
ArmKung <s.arm.kung@gmail.com>
2016-09-28 12:33:45 +0700

    Add action, constant

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import ui from './ui';
-import reducer from './action-reducer';
+import reducer, { updateUI, mountUI, unmountUI, setDefaultUI, UPDATE_UI_STATE, SET_DEFAULT_UI_STATE } from './action-reducer';
 
 export default ui;
 export { reducer };
+export { UPDATE_UI_STATE, SET_DEFAULT_UI_STATE };
+export const action = { updateUI, mountUI, unmountUI, setDefaultUI };
